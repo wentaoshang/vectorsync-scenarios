8982abde05a42e859cf84266958087295fcce300
Wentao Shang <wentaoshang@gmail.com>
2017-02-17 17:31:35 -0800

    measure data propagation delay in hub-and-spoke scenario

--- scenarios/hub-and-spoke.cpp
+++ scenarios/hub-and-spoke.cpp
@@ -2,6 +2,12 @@
 
 #include "simple-app.hpp"
 
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
 #include "ns3/core-module.h"
 #include "ns3/ndnSIM-module.h"
 #include "ns3/network-module.h"
@@ -11,6 +17,20 @@
 NS_LOG_COMPONENT_DEFINE("ns3.ndn.vsync.scenarios.HubAndSpoke");
 
 namespace ns3 {
+
+std::unordered_map<std::string, std::pair<double, std::vector<double>>> delays;
+
+static void DataEvent(std::shared_ptr<const ndn::Data> data, bool is_local) {
+  auto name = data->getName().toUri();
+  NS_LOG_INFO("new data: name=" << name << ", isLocal="
+                                << (is_local ? "true" : "false"));
+  double now = Simulator::Now().GetSeconds();
+  auto& entry = delays[name];
+  if (is_local)
+    entry.first = now;
+  else
+    entry.second.push_back(now);
+}
 
 static void VectorClockChange(const ::ndn::vsync::VersionVector& vc) {
   NS_LOG_INFO("vector_clock=" << vc);
@@ -23,20 +43,21 @@ static void ViewIDChange(const ::ndn::vsync::ViewID& vid) {
 int main(int argc, char* argv[]) {
   Config::SetDefault("ns3::PointToPointNetDevice::DataRate",
                      StringValue("10Mbps"));
-  Config::SetDefault("ns3::PointToPointChannel::Delay", StringValue("1ms"));
+  Config::SetDefault("ns3::PointToPointChannel::Delay", StringValue("10ms"));
   Config::SetDefault("ns3::DropTailQueue::MaxPackets", StringValue("100"));
 
   CommandLine cmd;
   cmd.Parse(argc, argv);
 
+  const int N = 10;
+  const double TotalRunTimeSeconds = 600.0;
+
   NodeContainer nodes;
-  nodes.Create(4);
+  nodes.Create(N + 1);
 
   // Node 0 is central hub
   PointToPointHelper p2p;
-  p2p.Install(nodes.Get(0), nodes.Get(1));
-  p2p.Install(nodes.Get(0), nodes.Get(2));
-  p2p.Install(nodes.Get(0), nodes.Get(3));
+  for (int i = 1; i <= N; ++i) p2p.Install(nodes.Get(0), nodes.Get(i));
 
   ndn::StackHelper ndnHelper;
   ndnHelper.SetDefaultRoutes(true);
@@ -45,31 +66,19 @@ int main(int argc, char* argv[]) {
   ndn::StrategyChoiceHelper::InstallAll(::ndn::vsync::kSyncPrefix.toUri(),
                                         "/localhost/nfd/strategy/multicast");
 
-  Ptr<UniformRandomVariable> x = CreateObject<UniformRandomVariable>();
+  Ptr<UniformRandomVariable> seed = CreateObject<UniformRandomVariable>();
 
-  ndn::AppHelper helper1("ns3::ndn::vsync::SimpleNodeApp");
-  helper1.SetAttribute("NodeID", StringValue("N1"));
-  helper1.SetAttribute("RandomSeed", UintegerValue(x->GetInteger()));
-  helper1.Install(nodes.Get(1)).Start(Seconds(1.0));
+  for (int i = 1; i <= N; ++i) {
+    ndn::AppHelper helper("ns3::ndn::vsync::SimpleNodeApp");
+    helper.SetAttribute("NodeID", StringValue("N" + std::to_string(i)));
+    helper.SetAttribute("RandomSeed", UintegerValue(seed->GetInteger()));
+    helper.SetAttribute("StartTime", TimeValue(Seconds(1.0)));
+    helper.SetAttribute("StopTime", TimeValue(Seconds(TotalRunTimeSeconds)));
+    helper.Install(nodes.Get(i));
 
-  ndn::AppHelper helper2("ns3::ndn::vsync::SimpleNodeApp");
-  helper2.SetAttribute("NodeID", StringValue("N2"));
-  helper2.SetAttribute("RandomSeed", UintegerValue(x->GetInteger()));
-  helper2.SetAttribute("StartTime", TimeValue(Seconds(10.0)));
-  helper2.SetAttribute("StopTime", TimeValue(Seconds(35.0)));
-  helper2.Install(nodes.Get(2));
-
-  ndn::AppHelper helper3("ns3::ndn::vsync::SimpleNodeApp");
-  helper3.SetAttribute("NodeID", StringValue("N3"));
-  helper3.SetAttribute("RandomSeed", UintegerValue(x->GetInteger()));
-  helper3.Install(nodes.Get(3)).Start(Seconds(20.0));
-
-  for (int i = 1; i <= 3; ++i) {
     ndn::FibHelper::AddRoute(nodes.Get(0), ::ndn::vsync::kSyncPrefix.toUri(),
                              nodes.Get(i), 1);
-    StringValue nid;
-    nodes.Get(i)->GetApplication(0)->GetAttribute("NodeID", nid);
-    std::string node_prefix = "/" + nid.Get();
+    std::string node_prefix = "/N" + std::to_string(i);
     ndn::FibHelper::AddRoute(nodes.Get(0), node_prefix, nodes.Get(i), 1);
 
     ndn::FibHelper::AddRoute(nodes.Get(i), "/", nodes.Get(0), 1);
@@ -80,12 +89,32 @@ int main(int argc, char* argv[]) {
         "VectorClock", MakeCallback(&VectorClockChange));
     nodes.Get(i)->GetApplication(0)->TraceConnectWithoutContext(
         "ViewID", MakeCallback(&ViewIDChange));
+    nodes.Get(i)->GetApplication(0)->TraceConnectWithoutContext(
+        "DataEvent", MakeCallback(&DataEvent));
   }
 
-  Simulator::Stop(Seconds(60.0));
+  Simulator::Stop(Seconds(TotalRunTimeSeconds));
 
   Simulator::Run();
   Simulator::Destroy();
+
+  int count = 0;
+  double average_delay = std::accumulate(
+      delays.begin(), delays.end(), 0.0,
+      [&count](double a, const decltype(delays)::value_type& b) {
+        double gen_time = b.second.first;
+        const auto& vec = b.second.second;
+        count += vec.size();
+        return a + std::accumulate(vec.begin(), vec.end(), 0.0,
+                                   [gen_time](double c, double d) {
+                                     return c + d - gen_time;
+                                   });
+      });
+  average_delay /= count;
+
+  std::cout << "Total number of data propagated is: " << count << std::endl;
+  std::cout << "Average data propagation delay is: " << average_delay
+            << " seconds." << std::endl;
 
   return 0;
 }
