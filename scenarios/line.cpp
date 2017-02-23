be4e69ef775fffa092f9908416aa00f7b049cddc
Wentao Shang <wentaoshang@gmail.com>
2017-02-23 15:42:31 -0800

    calculate data propagation delay in line scenario

--- scenarios/line.cpp
+++ scenarios/line.cpp
@@ -2,6 +2,12 @@
 
 #include "simple-app.hpp"
 
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
 #include "ns3/core-module.h"
 #include "ns3/ndnSIM-module.h"
 #include "ns3/network-module.h"
@@ -12,19 +18,21 @@ NS_LOG_COMPONENT_DEFINE("ns3.ndn.vsync.scenarios.Line");
 
 namespace ns3 {
 
+std::unordered_map<std::string, std::pair<double, std::vector<double>>> delays;
+
 static void DataEvent(std::string nid, std::shared_ptr<const ndn::Data> data,
                       bool is_local) {
   auto name = data->getName().toUri();
   NS_LOG_INFO("new_data_name=" << name << ", node_id=" << nid << ", is_local="
                                << (is_local ? "true" : "false"));
 
-  // double now = Simulator::Now().GetSeconds();
+  double now = Simulator::Now().GetSeconds();
 
-  // auto& entry = delays[name];
-  // if (is_local)
-  //   entry.first = now;
-  // else
-  //   entry.second.push_back(now);
+  auto& entry = delays[name];
+  if (is_local)
+    entry.first = now;
+  else
+    entry.second.push_back(now);
 }
 
 static void VectorClockChange(std::string nid, std::size_t idx,
@@ -41,8 +49,8 @@ static void ViewChange(std::string nid, const ::ndn::vsync::ViewID& vid,
 
 int main(int argc, char* argv[]) {
   Config::SetDefault("ns3::PointToPointNetDevice::DataRate",
-                     StringValue("10Mbps"));
-  Config::SetDefault("ns3::PointToPointChannel::Delay", StringValue("1ms"));
+                     StringValue("100Mbps"));
+  Config::SetDefault("ns3::PointToPointChannel::Delay", StringValue("10ms"));
   Config::SetDefault("ns3::DropTailQueue::MaxPackets", StringValue("100"));
 
   int N = 6;
@@ -117,6 +125,24 @@ int main(int argc, char* argv[]) {
 
   Simulator::Run();
   Simulator::Destroy();
+
+  int count = 0;
+  double average_delay = std::accumulate(
+      delays.begin(), delays.end(), 0.0,
+      [&count](double a, const decltype(delays)::value_type& b) {
+        double gen_time = b.second.first;
+        const auto& vec = b.second.second;
+        count += vec.size();
+        return a + std::accumulate(vec.begin(), vec.end(), 0.0,
+                                   [gen_time](double c, double d) {
+                                     return c + d - gen_time;
+                                   });
+      });
+  average_delay /= count;
+
+  std::cout << "Total number of data propagated is: " << count << std::endl;
+  std::cout << "Average data propagation delay is: " << average_delay
+            << " seconds." << std::endl;
 
   return 0;
 }
