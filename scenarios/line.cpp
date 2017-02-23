a87b2f04c929df9c90cd8c004e5c5cd07310cd43
Wentao Shang <wentaoshang@gmail.com>
2017-02-22 17:58:04 -0800

    add tracing callbacks to line scenario

--- scenarios/line.cpp
+++ scenarios/line.cpp
@@ -12,75 +12,108 @@ NS_LOG_COMPONENT_DEFINE("ns3.ndn.vsync.scenarios.Line");
 
 namespace ns3 {
 
+static void DataEvent(std::string nid, std::shared_ptr<const ndn::Data> data,
+                      bool is_local) {
+  auto name = data->getName().toUri();
+  NS_LOG_INFO("new_data_name=" << name << ", node_id=" << nid << ", is_local="
+                               << (is_local ? "true" : "false"));
+
+  // double now = Simulator::Now().GetSeconds();
+
+  // auto& entry = delays[name];
+  // if (is_local)
+  //   entry.first = now;
+  // else
+  //   entry.second.push_back(now);
+}
+
+static void VectorClockChange(std::string nid, std::size_t idx,
+                              const ::ndn::vsync::VersionVector& vc) {
+  NS_LOG_INFO("node_id=\"" << nid << "\", node_index=" << idx
+                           << ", vector_clock=" << vc);
+}
+
+static void ViewChange(std::string nid, const ::ndn::vsync::ViewID& vid,
+                       const ::ndn::vsync::ViewInfo& vinfo) {
+  NS_LOG_INFO("node_id=\"" << nid << "\", view_id=" << vid
+                           << ", view_info=" << vinfo);
+}
+
 int main(int argc, char* argv[]) {
   Config::SetDefault("ns3::PointToPointNetDevice::DataRate",
                      StringValue("10Mbps"));
   Config::SetDefault("ns3::PointToPointChannel::Delay", StringValue("1ms"));
   Config::SetDefault("ns3::DropTailQueue::MaxPackets", StringValue("100"));
 
+  int N = 6;
+  double TotalRunTimeSeconds = 60.0;
+  bool Synchronized = false;
+
   CommandLine cmd;
+  cmd.AddValue("NumOfNodes", "Number of sync nodes in the group (>= 2)", N);
+  cmd.AddValue("TotalRunTimeSeconds",
+               "Total running time of the simulation in seconds",
+               TotalRunTimeSeconds);
+  cmd.AddValue(
+      "Synchronized",
+      "If set, the data publishing events from all nodes are synchronized",
+      Synchronized);
   cmd.Parse(argc, argv);
 
+  if (N < 2) return -1;
+
   NodeContainer nodes;
-  nodes.Create(4);
+  nodes.Create(N);
 
   PointToPointHelper p2p;
-  p2p.Install(nodes.Get(0), nodes.Get(1));
-  p2p.Install(nodes.Get(1), nodes.Get(2));
-  p2p.Install(nodes.Get(2), nodes.Get(3));
+  for (int i = 0; i < N - 1; ++i) p2p.Install(nodes.Get(i), nodes.Get(i + 1));
 
   ndn::StackHelper ndnHelper;
-  ndnHelper.SetDefaultRoutes(true);
   ndnHelper.InstallAll();
 
-  ndn::StrategyChoiceHelper::InstallAll(::ndn::vsync::kSyncPrefix.toUri(),
+  ndn::StrategyChoiceHelper::InstallAll(::ndn::vsync::kSyncPrefix,
                                         "/localhost/nfd/strategy/multicast");
 
-  Ptr<UniformRandomVariable> x = CreateObject<UniformRandomVariable>();
+  ndn::GlobalRoutingHelper ndnGlobalRoutingHelper;
+  ndnGlobalRoutingHelper.InstallAll();
 
-  ndn::AppHelper helper0("ns3::ndn::vsync::SimpleNodeApp");
-  helper0.SetAttribute("NodeID", StringValue("N0"));
-  helper0.SetAttribute("RandomSeed", UintegerValue(x->GetInteger()));
-  helper0.Install(nodes.Get(0)).Start(Seconds(1.0));
+  Ptr<UniformRandomVariable> seed = CreateObject<UniformRandomVariable>();
+  seed->SetAttribute("Min", DoubleValue(0.0));
+  seed->SetAttribute("Max", DoubleValue(1000.0));
 
-  ndn::AppHelper helper1("ns3::ndn::vsync::SimpleNodeApp");
-  helper1.SetAttribute("NodeID", StringValue("N1"));
-  helper1.SetAttribute("RandomSeed", UintegerValue(x->GetInteger()));
-  helper1.Install(nodes.Get(1)).Start(Seconds(1.0));
+  for (int i = 0; i < N; ++i) {
+    ndn::AppHelper helper("ns3::ndn::vsync::SimpleNodeApp");
+    std::string nid = 'N' + std::to_string(i);
+    helper.SetAttribute("NodeID", StringValue(nid));
+    if (!Synchronized)
+      helper.SetAttribute("RandomSeed", UintegerValue(seed->GetInteger()));
+    helper.Install(nodes.Get(i)).Start(Seconds(1.0));
+    ndnGlobalRoutingHelper.AddOrigins('/' + nid, nodes.Get(i));
 
-  ndn::AppHelper helper2("ns3::ndn::vsync::SimpleNodeApp");
-  helper2.SetAttribute("NodeID", StringValue("N2"));
-  helper2.SetAttribute("RandomSeed", UintegerValue(x->GetInteger()));
-  helper2.Install(nodes.Get(2)).Start(Seconds(20.0));
+    nodes.Get(i)->GetApplication(0)->TraceConnect(
+        "VectorClock", nid, MakeCallback(&VectorClockChange));
+    nodes.Get(i)->GetApplication(0)->TraceConnect("ViewChange", nid,
+                                                  MakeCallback(&ViewChange));
+    nodes.Get(i)->GetApplication(0)->TraceConnect("DataEvent", nid,
+                                                  MakeCallback(&DataEvent));
+  }
 
-  ndn::AppHelper helper3("ns3::ndn::vsync::SimpleNodeApp");
-  helper3.SetAttribute("NodeID", StringValue("N3"));
-  helper3.SetAttribute("RandomSeed", UintegerValue(x->GetInteger()));
-  helper3.Install(nodes.Get(3)).Start(Seconds(1.0));
+  ndn::GlobalRoutingHelper::CalculateRoutes();
 
-  ndn::FibHelper::AddRoute(nodes.Get(0), ::ndn::vsync::kSyncPrefix.toUri(),
+  ndn::FibHelper::AddRoute(nodes.Get(0), ::ndn::vsync::kSyncPrefix,
                            nodes.Get(1), 1);
-  ndn::FibHelper::AddRoute(nodes.Get(0), "/", nodes.Get(1), 1);
 
-  ndn::FibHelper::AddRoute(nodes.Get(1), ::ndn::vsync::kSyncPrefix.toUri(),
-                           nodes.Get(0), 1);
-  ndn::FibHelper::AddRoute(nodes.Get(1), "/N0", nodes.Get(0), 1);
-  ndn::FibHelper::AddRoute(nodes.Get(1), ::ndn::vsync::kSyncPrefix.toUri(),
-                           nodes.Get(2), 1);
-  ndn::FibHelper::AddRoute(nodes.Get(1), "/", nodes.Get(2), 1);
+  for (int i = 1; i < N - 1; ++i) {
+    ndn::FibHelper::AddRoute(nodes.Get(i), ::ndn::vsync::kSyncPrefix,
+                             nodes.Get(i - 1), 1);
+    ndn::FibHelper::AddRoute(nodes.Get(i), ::ndn::vsync::kSyncPrefix,
+                             nodes.Get(i + 1), 1);
+  }
 
-  ndn::FibHelper::AddRoute(nodes.Get(2), ::ndn::vsync::kSyncPrefix.toUri(),
-                           nodes.Get(1), 1);
-  ndn::FibHelper::AddRoute(nodes.Get(2), "/", nodes.Get(1), 1);
-  ndn::FibHelper::AddRoute(nodes.Get(2), ::ndn::vsync::kSyncPrefix.toUri(),
-                           nodes.Get(3), 1);
-  ndn::FibHelper::AddRoute(nodes.Get(2), "/N3", nodes.Get(3), 1);
+  ndn::FibHelper::AddRoute(nodes.Get(N - 1), ::ndn::vsync::kSyncPrefix,
+                           nodes.Get(N - 2), 1);
 
-  ndn::FibHelper::AddRoute(nodes.Get(3), ::ndn::vsync::kSyncPrefix.toUri(),
-                           nodes.Get(2), 1);
-  ndn::FibHelper::AddRoute(nodes.Get(3), "/", nodes.Get(2), 1);
-
-  Simulator::Stop(Seconds(60.0));
+  Simulator::Stop(Seconds(TotalRunTimeSeconds));
 
   Simulator::Run();
   Simulator::Destroy();
