86cad12541ac25b4d8638f49207520a1ef6ddf1d
Wentao Shang <wentaoshang@gmail.com>
2017-04-20 21:36:03 -0700

    implement causal scenario

---
+++ extensions/simple-causal-app.cpp
@@ -0,0 +1,28 @@
+/* -*- Mode:C++; c-file-style:"google"; indent-tabs-mode:nil; -*- */
+
+#include "simple-causal-app.hpp"
+
+NS_LOG_COMPONENT_DEFINE("ns3.ndn.vsync.SimpleCOApp");
+
+namespace ns3 {
+namespace ndn {
+namespace vsync {
+
+NS_OBJECT_ENSURE_REGISTERED(SimpleCOApp);
+
+void SimpleCOApp::StartApplication() {
+  NS_LOG_INFO("NodeID: " << node_id_ << " Seed: " << seed_);
+  node_.reset(new ::ndn::vsync::app::SimpleCONode(
+      node_id_, routing_prefix_, ndn::StackHelper::getKeyChain(), seed_));
+  node_->ConnectVectorClockTrace(
+      std::bind(&SimpleCOApp::TraceVectorClock, this, _1, _2));
+  node_->ConnectViewChangeTrace(
+      std::bind(&SimpleCOApp::TraceViewChange, this, _1, _2, _3));
+  node_->ConnectDataEventTrace(
+      std::bind(&SimpleCOApp::TraceDataEvent, this, _1, _2));
+  node_->Start();
+}
+
+}  // namespace vsync
+}  // namespace ndn
+}  // namespace ns3
